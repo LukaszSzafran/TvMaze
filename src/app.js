1743de74f09ec0bae51a10e6aa671a48ce70f370
Łukasz Szafran <szafran@interia.pl>
2022-02-11 13:07:07 +0100

    Added posibility to change displayed shows and added validation

--- src/app.js
+++ src/app.js
@@ -11,8 +11,8 @@ class TvMaze {
 
   initializeApp = () => {
     this.connectDOMElements();
-    this.setupListeners()
-    this.fetchAndDisplayShows()
+    this.setupListeners();
+    this.fetchAndDisplayShows();
   };
 
   connectDOMElements = () => {
@@ -23,47 +23,71 @@ class TvMaze {
       document.querySelectorAll("[data-show-name]")
     ).map((elem) => elem.dataset.showName);
 
-   this.viewElems = mapListToDOMElements(listOFIds, 'id');
-    this.showNameButtons = mapListToDOMElements(listOfShowNames, 'data-show-name');
+    this.viewElems = mapListToDOMElements(listOFIds, "id");
+    this.showNameButtons = mapListToDOMElements(
+      listOfShowNames,
+      "data-show-name"
+    );
   };
 
   setupListeners = () => {
-    Object.keys(this.showNameButtons).forEach(
-      (showName => { this.showNameButtons[showName].addEventListener('click', this.setCurrentNameFilter) })
-    );
-  }
+    Object.keys(this.showNameButtons).forEach((showName) => {
+      this.showNameButtons[showName].addEventListener(
+        "click",
+        this.setCurrentNameFilter
+      );
+    });
+  };
 
   setCurrentNameFilter = () => {
-    this.selectedName = event.target.dataset.showName
-    this.fetchAndDisplayShows()
-  }
+    this.selectedName = event.target.dataset.showName;
+    this.fetchAndDisplayShows();
+  };
 
   fetchAndDisplayShows = () => {
-    getShowsByKey(this.selectedName).then(shows => this.renderCards(shows))
-  }
+    getShowsByKey(this.selectedName).then((shows) => this.renderCards(shows));
+  };
 
-  renderCards = shows => {
+  renderCards = (shows) => {
+    this.viewElems.showsWrapper.innerHTML = "";
+
     for (const { show } of shows) {
-      this.createShowCard(show)
+      this.createShowCard(show);
     }
-  }
+  };
 
-  createShowCard = show => {
-    const divCard = createDOMElem('div', 'card')
-    const img = createDOMElem("img", 'card-img-top', null, show.image.medium);
-    const divCardBody = createDOMElem('div', 'card-body')
-    const h5 = createDOMElem('h5', 'card-title', show.name)
-    const p = createDOMElem('p', 'card-text', show.summary)
-    const btn = createDOMElem('button', 'btn btn-primary', 'Show details')
+  createShowCard = (show) => {
+    const divCard = createDOMElem("div", "card");
+    const divCardBody = createDOMElem("div", "card-body");
+    const h5 = createDOMElem("h5", "card-title", show.name);
+    const btn = createDOMElem("button", "btn btn-primary", "Show details");
+    let img, p
 
-    divCard.appendChild(divCardBody)
-    divCardBody.appendChild(img)
-    divCardBody.appendChild(h5)
-    divCardBody.appendChild(p)
-    divCardBody.appendChild(btn)
+    if (show.image) {
+      img = createDOMElem("img", "card-img-top", null, show.image.medium);
+    } else {
+      img = createDOMElem(
+        "img",
+        "card-img-top",
+        null,
+        "https://via.placeholder.com/210x295"
+      );
+    }
 
-    this.viewElems.showsWrapper.appendChild(divCard)
-  }
+    if (show.summary) {
+      p = createDOMElem("p", "card-text", `${show.summary.slice(0, 80)}...`);
+    } else {
+      p = createDOMElem("p", "card-text", 'There is no summary for that show yet.');
+    }
+
+    divCard.appendChild(divCardBody);
+    divCardBody.appendChild(img);
+    divCardBody.appendChild(h5);
+    divCardBody.appendChild(p);
+    divCardBody.appendChild(btn);
+
+    this.viewElems.showsWrapper.appendChild(divCard);
+  };
 }
 
 document.addEventListener("DOMContentLoaded", new TvMaze());
